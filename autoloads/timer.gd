35b903999e43751aa739d7ba1eee85325ffc223d
DESKTOP-70883DV\gabur <daniuriza@outlook.com>
2026-07-25 12:26:03 -0400

    power up counter logic

--- autoloads/timer.gd
+++ autoloads/timer.gd
@@ -7,7 +7,7 @@ var current_max_time: float
 var half_time: float;
 var _currentTime: float = 0;
 
-var _reverse_time: bool = false;
+var _time_is_reversed: bool = false;
 signal max_time_wasChanged(new_max_time: float)
 signal delta_time_percentage(delta_time_percent: float)
 signal currentTime_percentage(_current_time_percent: float)
@@ -25,7 +25,7 @@ func _process(dt: float) -> void:
 
 	var delta_time = 0;
 
-	if !_reverse_time:
+	if !_time_is_reversed:
 		delta_time = dt;
 	else:
 		delta_time = - dt;
@@ -33,7 +33,7 @@ func _process(dt: float) -> void:
 	_emit_delta_time_as_percentage(delta_time);
 	_currentTime += delta_time
 	currentTime_percentage.emit(get_time_as_percentage());
-	print(get_time_as_percentage())
+	# print(get_time_as_percentage())
 	if _currentTime >= current_max_time or _currentTime < 0:
 		time_out.emit();
 		running = false
@@ -49,12 +49,12 @@ func get_time_as_percentage() -> float:
 	return _currentTime / current_max_time;
 
 func get_remaining_total_time_ratio() -> float:
-	print(current_max_time / initial_max_time)
+	# print(current_max_time / initial_max_time)
 	return current_max_time / initial_max_time
 
 func reverse_time() -> void:
-	_reverse_time = !_reverse_time;
-	time_reversed.emit();
+	_time_is_reversed = !_time_is_reversed;
+	time_reversed.emit(_time_is_reversed);
 
 func reset() -> void:
 	set_max_time(initial_max_time)
@@ -64,3 +64,6 @@ func reset() -> void:
 
 func _emit_delta_time_as_percentage(delta_time: float) -> void:
 	delta_time_percentage.emit(delta_time / current_max_time);
+
+func get_reversed_time() -> bool:
+	return _time_is_reversed;
